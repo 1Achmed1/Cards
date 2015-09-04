f4cff276183982387969064957d9bf692d86a926
1Achmed1 <tmiandreww@gmail.com>
2015-09-04 18:52:38 -0400

    Update base.js

--- base.js
+++ base.js
@@ -19,11 +19,6 @@ var main = function() {
   $('.header-link-container4').click(function() {
     window.location.href = href4;
   });
-
-  if(navigator.userAgent.indexOf('Mac OS X') != -1) {
-    $(".left-part").css('margin-top', '8px');
-    $(":li[active]").css('padding-bottom', '25px')
-  }
 }
 
 var footerToggle = function() {
@@ -71,7 +66,7 @@ $(window).scroll(function() {
       header.attr('class', 'card header');
       if(navigator.userAgent.indexOf('Mac OS X') != -1) {
         $(".left-part").css('margin-top', '8px');
-        $(":li[active]").css('padding-bottom', '25px')
+        $("li[active]").css('padding-bottom', '25px');
       }
 
       if($(".headerImg").length) {
